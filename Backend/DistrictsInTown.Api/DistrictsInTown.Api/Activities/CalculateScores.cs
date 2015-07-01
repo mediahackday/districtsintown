9d5e228b8eee8351e141c6e8e2438736ed247120
Florian Luther <Florian.Luther@live.de>
2015-07-01 16:59:11 +0200

    ADDED: place score calculation.

---
+++ Backend/DistrictsInTown.Api/DistrictsInTown.Api/Activities/CalculateScores.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DistrictsInTown.Api.Models;
+
+namespace DistrictsInTown.Api.Activities
+{
+    public class CalculateScores
+    {
+        public IEnumerable<Place> For(IEnumerable<Place> places)
+        {
+            var placesList = places.ToList();
+            var zipCodes = placesList.Select(z => z.ZipCode).Distinct();
+
+            foreach (var zipCode in zipCodes)
+            {
+                var code = zipCode;
+                var filteredByZipCode = placesList.Where(z => z.ZipCode == code).ToList();
+
+                yield return new Place
+                {
+                    Score = filteredByZipCode.Average(s => s.Score),
+                    Latitude = filteredByZipCode.Average(l => l.Latitude),
+                    Longitude = filteredByZipCode.Average(l => l.Longitude),
+                    ZipCode = code
+                };
+            }
+        }
+    }
+}
