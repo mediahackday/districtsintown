d707ddec530963a7f00009a36666e312ab1cd21b
burnstreet <github@burnstreet.de>
2015-07-01 13:52:09 +0200

    ADDED: Basic info

--- Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
+++ Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
@@ -8,13 +8,11 @@ namespace IPoolImporter
 {
     class Program
     {
-        private const string API_KEY = "mediahackday";
-        private const string API_SECRET = "1hackpool!";
         internal const string BASEURL = "https://ipool.s.asideas.de:443/api/v3/";
 
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
