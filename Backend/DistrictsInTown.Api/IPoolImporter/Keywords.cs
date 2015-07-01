4018a9358d0fb8c4e1b05fb5e89fe760f40e98b4
burnstreet <github@burnstreet.de>
2015-07-01 20:49:09 +0200

    Loading articles works

--- Backend/DistrictsInTown.Api/IPoolImporter/Keywords.cs
+++ Backend/DistrictsInTown.Api/IPoolImporter/Keywords.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CsvHelper;
 
 namespace IPoolImporter
 {
-    class Keywords
+    internal class Keywords
     {
-        public readonly Dictionary<string, int> Scores = new Dictionary<string, int>(); 
+        public readonly Dictionary<string, int> Scores = new Dictionary<string, int>();
 
+        public Keywords()
+        {
+            var reader = File.OpenText("Data\\histogram.tsv.txt");
+            var csv = new CsvReader(reader);
+            csv.Configuration.Delimiter = "\t";
+
+            var input = csv.GetRecords<Record>();
+            foreach (var record in input)
+            {
+                Scores[record.Keyword] = record.Score;
+            }
+        }
+
+        private class Record
+        {
+            public string Keyword { get; set; }
+            public int Frequency { get; set; }
+            public int Score { get; set; }
+        }
     }
 }
--- Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
+++ Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
@@ -13,52 +13,73 @@ namespace IPoolImporter
         internal const string API_KEY = "mediahackday";
         internal const string API_SECRET = "1hackpool!";
         internal const string BASEURL = "https://ipool.s.asideas.de:443/api/v3/";
-        internal static readonly string[] BEZIRKE = 
+
+
+        internal static readonly Dictionary<string, string> Bezirke = new Dictionary<string, string>()
         {
-            "friedrichshain",
-            "Friedrichshain-Kreuzberg",
-            "kreuzberg",
-            "reinickendorf",
-            "Reinickendorf",
-            "Mitte",
-            "charlottenburg",
-            "OTS_Charlottenburg-Wilmersdorf",
-            "Charlottenburg-Wilmersdorf",
-            "steglitz",
-            "Steglitz-Zehlendorf",
-            "zehlendorf",
-            "tempelhof",
-            "Tempelhof-SchÃ¶neberg",
-            "Tempelhof-Schöneberg",
-            "spandau",
-            "Spandau",
-            "NeukÃ¶lln",
-            "neukolln",
-            "treptow",
-            "Treptow-Köpenick",
-            "OTS_Treptow-KÃ¶penick",
-            "Treptow-KÃ¶penick",
-            "penick",
-            "köpenick",
-            "kopenick",
-            "marzahn",
-            "Marzahn-Hellersdorf",
-            "hellersdorf",
-            "lichtenberg",
-            "Lichtenberg",
-        };
+            { "Mitte", "POINT (52.516666666667 13.366666666667)" },
+		    { "friedrichshain", "POINT (52.5 13.402222222222)" },
+		    { "Friedrichshain-Kreuzberg", "POINT (52.5 13.402222222222)" },
+		    { "kreuzberg", "POINT (52.5 13.402222222222)" },
+		    { "Pankow", "POINT (52.568888888889 13.402222222222)" },
+		    { "charlottenburg", "POINT (52.498888888889 13.285)" },
+		    { "OTS_Charlottenburg-Wilmersdorf", "POINT (52.498888888889 13.285)" },
+		    { "Charlottenburg-Wilmersdorf", "POINT (52.498888888889 13.285)" },
+		    { "Spandau", "POINT (52.533333333333 13.166666666667)" },
+		    { "spandau", "POINT (52.533333333333 13.166666666667)" },
+		    { "steglitz", "POINT (52.433333333333 13.25)" },
+		    { "zehlendorf", "POINT (52.433333333333 13.25)" },
+		    { "Steglitz-Zehlendorf", "POINT (52.433333333333 13.25)" },
+		    { "tempelhof", "POINT (52.466666666667 13.383333333333)" },
+		    { "Tempelhof-SchÃ¶neberg", "POINT (52.466666666667 13.383333333333)" },
+		    { "Tempelhof-Schöneberg", "POINT (52.466666666667 13.383333333333)" },
+		    { "NeukÃ¶lln", "POINT (52.483333333333 13.45)" },
+		    { "neukolln", "POINT (52.483333333333 13.45)" },
+		    { "treptow", "POINT (52.45 13.566666666667)" },
+		    { "Treptow-Köpenick", "POINT (52.45 13.566666666667)" },
+		    { "OTS_Treptow-KÃ¶penick", "POINT (52.45 13.566666666667)" },
+		    { "penick", "POINT (52.45 13.566666666667)" },
+		    { "köpenick", "POINT (52.45 13.566666666667)" },
+		    { "kopenick", "POINT (52.45 13.566666666667)" },
+		    { "Treptow-KÃ¶penick", "POINT (52.45 13.566666666667)" },
+		    { "marzahn", "POINT (52.539722222222 13.584166666667)" },
+		    { "Marzahn-Hellersdorf", "POINT (52.539722222222 13.584166666667)" },
+		    { "hellersdorf", "POINT (52.539722222222 13.584166666667)" },
+		    { "Lichtenberg", "POINT (52.533333333333 13.5)" },
+		    { "lichtenberg", "POINT (52.533333333333 13.5)" },
+		    { "Reinickendorf", "POINT (52.566666666667 13.35)" },
+ 		    { "reinickendorf", "POINT (52.566666666667 13.35)" },
+       }; 
         
 
         static void Main(string[] args)
         {
-            GetHistogram();
+            var news = GetNewsForDistricts();
+            new Saver().Save(news);
+        }
+
+        private static Dictionary<string, List<News>> GetNewsForDistricts()
+        {
+            var client = new ArticleClient();
+            Dictionary<string, List<News>> news = new Dictionary<string, List<News>>();
+            foreach (var kv in Bezirke)
+            {
+                if (!news.ContainsKey(kv.Value))
+                {
+                    Console.WriteLine(kv.Key);
+                    news[kv.Value] = new List<News>();
+                    var districtNews = client.GetNewsForDistrict(kv.Key);
+                    news[kv.Value].AddRange(districtNews);
+                }
+            }
+            return news;
         }
 
         private static void GetHistogram()
         {
             var client = new ArticleClient();
             Dictionary<string, int> hist = new Dictionary<string, int>();
-            foreach (var district in BEZIRKE)
+            foreach (var district in Bezirke.Keys)
             {
                 Console.WriteLine(district);
                 client.GetHistogram(district, hist);
@@ -74,7 +95,7 @@ namespace IPoolImporter
         private static void GetKeywordsForDistricts()
         {
             var client = new ArticleClient();
-            foreach (var district in BEZIRKE)
+            foreach (var district in Bezirke.Keys)
             {
                 Console.WriteLine(district);
                 var result = client.GetKeywordsForDistrict(district);
@@ -89,11 +110,24 @@ namespace IPoolImporter
         private static void GetNumArticlesForDistricts()
         {
             var client = new ArticleClient();
-            foreach (var district in BEZIRKE)
+            foreach (var district in Bezirke.Keys)
             {
                 client.GetNumArticlesForDistrict(district);
             }
             Console.ReadLine();
+        }
+    }
+
+    internal class News
+    {
+        public string ID { get; set; }
+        public decimal RawScore { get; set; }
+
+        public decimal GetScore(decimal min, decimal max)
+        {
+            var nulledScore = RawScore - min;
+            var multiplier = 10/(max - min);
+            return nulledScore*multiplier;
         }
     }
 }
