02fa1aaf3a919eccef01f796c16f255841fecff8
burnstreet <github@burnstreet.de>
2015-07-01 15:34:32 +0200

    Working OAuth

--- Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
+++ Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IPoolImporter.Client;
 
 namespace IPoolImporter
 {
     class Program
     {
+        internal const string API_KEY = "mediahackday";
+        internal const string API_SECRET = "1hackpool!";
         internal const string BASEURL = "https://ipool.s.asideas.de:443/api/v3/";
+        internal readonly string[] BEZIRKE = 
+        {
+            "friedrichshain",
+            "kreuzberg",
+            "reinickendorf",
+            "mitte",
+            "charlottenburg",
+            "steglitz",
+            "tempelhof"
+        };
 
         static void Main(string[] args)
         {
-            
+            var client = new ArticleClient(API_KEY, API_SECRET);
+            client.GetNumArticlesForDistrict("blubb");
+
+            Console.ReadLine();
         }
     }
 }
