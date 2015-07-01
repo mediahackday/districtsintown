11ae4acbf0a15726e26bbf21fc859dc00db0a854
Aurelius Wendelken <webtobesocial@gmail.com>
2015-07-01 23:00:46 +0200

    removed unused code and formated code

--- web/ui/map/map.js
+++ web/ui/map/map.js
@@ -5,23 +5,26 @@ var app = angular.module('DistrictsInTown');
 app.controller('mapCtrl', function($scope, DistrictServ) {
 	var heatmap = null;
 
-  $scope.updateHeatMap = function(event) {
-    if (event.charCode === 32 || event.charCode === 0) {
-      DistrictServ.getLocationData($scope.keywords).then(function (d) {
+	$scope.updateHeatMap = function(event) {
+		if (event.charCode === 32 || event.charCode === 0) {
+			DistrictServ.getLocationData($scope.keywords).then(function (d) {
 				if (map.hasLayer(heatmap)) {
 					map.removeLayer(heatmap);
 				}
-        if(d.data.data) {
-					  heatmap = new L.TileLayer.WebGLHeatMap({size: 1000, autoresize: true});
-						for (var i = 0, len = d.data.data.length; i < len; i++) {
-    					var point = d.data.data[i];
-    					heatmap.addDataPoint(point.lat,point.lng,point.count*8);
-						}
-						map.addLayer(heatmap);
-        }});
-    }
-  };
-	var map = L.map('map').setView([52.5247, 13.38885], 10);
+
+				if (d.data.data) {
+					heatmap = new L.TileLayer.WebGLHeatMap({size: 1000, autoresize: true});
+					for (var i = 0, len = d.data.data.length; i < len; i++) {
+						var point = d.data.data[i];
+						heatmap.addDataPoint(point.lat,point.lng,point.count*8);
+					}
+					map.addLayer(heatmap);
+				}
+			});
+		}
+	};
+
+	var map = L.map('map').setView([52.5247, 13.38885], 12);
 
 	var tiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
--- web/ui/services/districtService.js
+++ web/ui/services/districtService.js
@@ -3,29 +3,21 @@
 var app = angular.module('DistrictsInTown');
 
 app.factory('DistrictServ', function($http) {
+	return {
+		getLocationData: function (keywords) {
+			keywords = keywords.split(" ");
+			var url = 'http://districtsintown.azurewebsites.net/api/place?';
 
-  return {
-            getFakeLocationData: function () {
-                var res =  $http({
-                    url: "http://localhost:9000/fake.json",
-                    method: "GET"
-                 });
+			for (var i = 0; i < keywords.length; i++) {
+				url = url + 'keyword=' + keywords[i] + '&';
+			}
 
-                return res;
-            },
+			var res =  $http({
+				url: url,
+				method: "GET"
+			});
 
-            getLocationData: function (keywords) {
-                keywords = keywords.split(" ");
-                var url = 'http://districtsintown.azurewebsites.net/api/place?';
-                for (var i = 0; i < keywords.length; i++) {
-                  url = url + 'keyword=' + keywords[i] + '&';
-                }
-                var res =  $http({
-                    url: url,
-                    method: "GET"
-                 });
-
-                return res;
-              }
-            };
-  });
+			return res;
+		}
+	};
+});
