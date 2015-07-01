4249e038f351e72c78a195ab8e5166c45828af15
Florian Luther <Florian.Luther@live.de>
2015-07-01 12:50:20 +0200

    ADDED: location data model, controller and repository. CHANGED: WebApiConfig -> modified route. CHANGED: ignored files and folders.

---
+++ Backend/DistrictsInTown.Api/DistrictsInTown.Api/Models/Data.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DistrictsInTown.Api.Models
+{
+    public class Data
+    {
+        [JsonProperty("max")]
+        public int Max { get; set; }
+
+        // TODO: max 40k items
+        [JsonProperty("data")]
+        public IEnumerable<Location> Locations { get; set; }
+    }
+}
