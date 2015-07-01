92d40d9032e3fabde8939881c0e96a4f95567c78
Ryan Hendry <ryanhendry123@gmail.com>
2015-07-01 11:41:20 +0200

    renaming files, removing unessesary files and adding service folder

--- web/ui/components/version/version-directive_test.js
+++ web/ui/components/version/version-directive_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('DistrictsInTown.version module', function() {
+  beforeEach(module('DistrictsInTown.version'));
 
   describe('app-version directive', function() {
     it('should print current version', function() {
--- web/ui/map/map.js
+++ web/ui/map/map.js
@@ -1,15 +1,15 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('DistrictsInTown.map', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/map', {
+    templateUrl: 'map/map.html',
+    controller: 'mapCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('mapCtrl', [function() {
 	var map = L.map('map').setView([51.505, -0.09], 13);
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -17,9 +17,9 @@ angular.module('myApp.view1', ['ngRoute'])
 		id: 'webtobesocial.jokblmno',
 		accessToken: 'pk.eyJ1Ijoid2VidG9iZXNvY2lhbCIsImEiOiJaU3NfOVdRIn0.k0Zr0K8bPDstktQYhXY-ZA'
 	}).addTo(map);
-	
+
 	map.locate({setView: true, maxZoom: 16});
-	
+
 	function onLocationFound(e) {
 		var radius = e.accuracy / 2;
 
@@ -30,7 +30,7 @@ angular.module('myApp.view1', ['ngRoute'])
 	}
 
 	map.on('locationfound', onLocationFound);
-	
+
 	function onLocationError(e) {
 		alert(e.message);
 	}
