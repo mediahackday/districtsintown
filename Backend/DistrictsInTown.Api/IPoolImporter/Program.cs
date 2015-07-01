d018d4fcdb1e8f166bc183509f69f572144bc09a
burnstreet <github@burnstreet.de>
2015-07-01 12:46:25 +0200

    ADDED: IPoolImporter Project

---
+++ Backend/DistrictsInTown.Api/IPoolImporter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPoolImporter
+{
+    class Program
+    {
+        private const string API_KEY = "mediahackday";
+        private const string API_SECRET = "1hackpool!";
+        internal const string BASEURL = "https://ipool.s.asideas.de:443/api/v3/";
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
